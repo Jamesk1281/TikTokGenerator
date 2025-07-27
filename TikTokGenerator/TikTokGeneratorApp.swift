d9d3bac5bfa5660a8b22da0da7fba27d3aded7b3
Jamesk1281 <26jkouvlis@xbhs.net>
2025-07-27 12:45:03 -0400

    Initial Commit

---
+++ TikTokGenerator/TikTokGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokGeneratorApp.swift
+//  TikTokGenerator
+//
+//  Created by James . on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TikTokGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
